99a3232e1ad88d884b055b7fc2f561b118c80579
anxin <anxin@anxins-MacBook-Pro.local>
2019-01-10 13:04:39 +0800

    install script

---
+++ src/scripts/install_dependency.sh
@@ -0,0 +1,110 @@
+#!/usr/bin/env bash
+
+installGoPackage() {
+	echo "installing go package"
+	export GOPATH=$(dirname $(dirname "$PWD"))
+	go get -u github.com/ethereum/go-ethereum
+	go get -u github.com/go-redis/redis
+	go get -u github.com/gorilla/handlers
+	go get -u github.com/gorilla/mux
+	go get -u github.com/gorilla/websocket
+	go get -u github.com/goware/disque
+	echo "finish installing go package"
+}
+
+installGO() {
+	echo "installing go"
+	if [ -x "$(command -v go)" ];
+		echo "have installed go"
+		return
+	fi 
+
+	if ["$(uname)"=="Darwin"];
+	then
+		brew update
+		brew install golang
+	elif ["$(expr substr $(uname -s) 1 5)"=="Linux"];
+	then
+		sudo snap install go --classic
+	else
+		echo "do not support this system"
+		exit 1
+	fi
+
+	echo "finish installing go"
+}
+
+installGeth() {
+	echo "installing geth"
+	if [ -x "$(command -v geth)" ];
+		echo "have installed geth,skip"
+		return
+	fi 
+
+	if ["$(uname)"=="Darwin"];
+	then
+		brew tap ethereum/ethereum
+		brew install ethereum
+	elif ["$(expr substr $(uname -s) 1 5)"=="Linux"];
+	then
+		sudo add-apt-repository -y ppa:ethereum/ethereum
+		sudo apt-get update
+		sudo apt-get install -y ethereum
+	else
+		echo "do not support this system"
+		exit 1
+	fi
+	echo "finish installing geth"
+}
+
+installRedis() {
+	echo "installing redis"
+	#install in $HOME
+
+	if [ -x "$(command -v redis-server)" ]
+	then 
+		echo "have installed redis skip"
+		return
+	fi
+
+	(	
+		cd $HOME
+		curl -O http://download.redis.io/releases/${target}
+		tar -xzvf ${target}
+		cd $dst
+		make
+		ln -s $(pwd)/src/redis-server /usr/local/bin/redis-server
+	)
+
+	echo "finish installing redis"
+}
+
+installNodeJS() {
+	echo "installing node js"
+	if [ -x "$(command -v node)" ];
+		echo "have installed geth,skip"
+		return
+	fi
+
+	if ["$(uname)"=="Darwin"];
+	then
+		brew update
+		brew install node
+	elif ["$(expr substr $(uname -s) 1 5)"=="Linux"];
+	then
+		curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
+		sudo apt install -y nodejs
+	else
+		echo "do not support this system"
+		exit 1
+	fi
+}
+
+installGeth 
+installGO
+installGoPackage
+installRedis
+installNodeJS
+
+
+
